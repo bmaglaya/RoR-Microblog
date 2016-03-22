9b2cc4434eebdd7260a93c930cd1691cf57bcf02
Blase Maglaya <blasemaglaya@gmail.com>
2016-03-22 07:08:07 +0000

    Display user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,4 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-                    
 end
